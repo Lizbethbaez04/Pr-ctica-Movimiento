44b4c30d50bfff36975d191ada1beabb457d0b01
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-10-26 07:47:20 -0700

    Agregar archivos de proyecto.

---
+++ PracticaMovimiento/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticaMovimiento
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            miCanvas.Focus();
+        }
+
+        private void miCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Up)
+            {
+                //asi es como se mueve la imagen si aprieta el boton "arriba"
+                double topPandaActual = Canvas.GetTop(imgPanda);
+                Canvas.SetTop(imgPanda, topPandaActual - 15);
+            }
+        }
+    }
+}
